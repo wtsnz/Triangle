3b373e63e4095dcc7cb650fa9bbdc3bc006f90c8
Will Townsend <will@townsend.io>
2018-10-24 18:05:14 -0700

    Initial Commit

---
+++ Triangle/Triangle.h
@@ -0,0 +1,19 @@
+//
+//  Triangle.h
+//  Triangle
+//
+//  Created by Will Townsend on 2018-10-24.
+//  Copyright © 2018 Will Townsend. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Triangle.
+FOUNDATION_EXPORT double TriangleVersionNumber;
+
+//! Project version string for Triangle.
+FOUNDATION_EXPORT const unsigned char TriangleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Triangle/PublicHeader.h>
+
+
